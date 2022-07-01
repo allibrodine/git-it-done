e2e2a953d98b58137843ed55279a87c9d2e4b274
allibrdoine <allibrodine81@gmail.com>
2022-07-01 17:04:06 -0500

    link pages/query perameters

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,21 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function() {
+    //grab repo name from url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+    
+    if(repoName) {
+        //display repo name on the page
+        repoNameEl.textContent = repoName;
+        getRepoIssues(repoName);
+    } else {
+        //if no repo was given, redirect to the homepage
+        document.location.replace("./index.html");
+    }
+};
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -17,12 +33,11 @@ var getRepoIssues = function(repo) {
                 }
             });
         } else {
-            alert("There was a problem with your request!");
+            //if not successful, redirect to the homepage
+            document.location.replace("./index.html");
         }
     });
 };
-
-getRepoIssues("facebook/react");
 
 var displayIssues = function(issues) {
     //if there are no issues
@@ -74,3 +89,5 @@ var displayWarning = function(repo) {
     //append to warning container
     limitWarningEl.appendChild(linkEl);
 };
+
+getRepoName();
